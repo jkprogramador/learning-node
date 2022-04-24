74de7827513efe6ff8f6a8fbe1e69658e84e1241
Jack Lee <jkprogramador@gmail.com>
2022-04-24 16:01:36 -0300

    Implement dynamic views with Handlebars.

--- app/meadowlark.js
+++ app/meadowlark.js
@@ -1,34 +1,39 @@
 const express = require('express')
 
+const expressHandlebars = require('express-handlebars')
+
 const app = express()
+
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine', 'handlebars')
 
 const port = process.env.NODE_PORT || 3000
 
 app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send('Meadowlark Travel')
+    res.render('home')
 })
 
 app.get('/about', (req, res) => {
-    res.type('text/plain')
-    res.send('About Meadowlark Travel')
+    res.render('about')
 })
 
 // custom 404 page
 // app.use is the method by which Express adds middleware
 app.use((req, res) => {
-    res.type('text/plain')
     res.status(404)
-    res.send('404 - Not Found')
+    res.render('404')
 })
 
 // custom 500 page
 // Express can distinguish between 404 and 500 by the number of arguments in the callback function
 app.use((err, req, res, next) => {
     console.error(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - Internal Server Error')
+    res.render('500')
 })
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}`))
