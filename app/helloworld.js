1d022f8da50c482c01811b8207b16925e5a6583d
Jack Lee <jkprogramador@gmail.com>
2022-04-24 10:14:28 -0300

    Add basic routing to helloworld.

--- app/helloworld.js
+++ app/helloworld.js
@@ -1,9 +1,25 @@
-const http = require('http')
+import http from 'http'
 
 const port = process.env.NODE_PORT || 3000
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello World')
+
+    // Normalize URL by removing querystring, optional trailing slash and making it lowercase
+    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+
+    switch (path) {
+        case '':
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end('Homepage')
+            break
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end('About')
+            break
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' })
+            res.end('Not Found')
+            break
+    }
 })
 
 server.listen(port, () => console.log(`server started on port ${port}`))
