2b1039d8577345c8faec692df0971f3977068803
Jack Lee <jkprogramador@gmail.com>
2022-04-24 22:34:35 -0300

    Implement static middleware to serve static files.

--- app/meadowlark.js
+++ app/meadowlark.js
@@ -13,13 +13,27 @@ app.set('view engine', 'handlebars')
 
 const port = process.env.NODE_PORT || 3000
 
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+]
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+    res.render('about', { fortune: randomFortune })
 })
+
+// Add static middleware. This will override other routes
+// It has the same effect as creating a route for each static file
+// The "public" directory is omitted when referencing files in HTML
+app.use(express.static(__dirname + '/public'))
 
 // custom 404 page
 // app.use is the method by which Express adds middleware
